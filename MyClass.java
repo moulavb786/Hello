876939425497a3c6ef9c6b7b56495ecc15005226
moulavb786 <59923839+moulavb786@users.noreply.github.com>
2020-10-25 14:18:24 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,18 @@
+package MyPack;
+
+public class MyClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Hello, World!"); 
+			 System.out.println("Hello, World!"); 
+			}
+		 
+		 
+
+	}
+
+}
